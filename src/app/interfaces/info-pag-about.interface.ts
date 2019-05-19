3fcd6abd876101f808ab96f7d0b1d5e923f0e871
Jose lopez <lopezajoseg@gmail.com>
2019-05-19 01:40:27 -0400

    Conexion con Firebase

---
+++ src/app/interfaces/info-pag-about.interface.ts
@@ -0,0 +1,7 @@
+export interface InfoPagAbout {
+  frase?: string;
+  nombre?: string;
+  subtitulo?: string;
+  twitter?: string;
+  url?: string;
+}
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,21 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { InfoPagAbout } from '../interfaces/info-pag-about.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
   info: InfoPagina ={};
+  equipotrab: InfoPagAbout={};
   cargada = false;
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     //console.log("Servicio InfoPaginaService activo");
     // Leer archivo ./assets/data/data-pagina.json JSON
     this.http.get('/assets/data/data-pagina.json')
     .subscribe((resp:InfoPagina) =>{
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
+      //console.log(resp);
+      //console.log(resp['twitter']);
+    });
+  }
+
+  private cargarEquipo(){
+    //console.log("Servicio InfoPaginaService activo");
+    // Leer archivo ./assets/data/data-pagina.json JSON
+    this.http.get('https://portafolio-d8e89.firebaseio.com/equipo.json')
+    .subscribe((resp:InfoPagAbout) =>{
+      this.cargada = true;
+      this.equipotrab = resp;
+      this.equipo[0] = resp;
+      console.log(this.equipotrab);
       //console.log(resp['twitter']);
     });
   }
